4abe7ca904000997ff13143b8aeabe7c08eae181
Zachary Wilson <wilsonzachary634@gmail.com>
2020-07-28 15:23:59 -0600

    pre commit

--- src/app/generate_pdf.js
+++ src/app/generate_pdf.js
@@ -1,16 +1,17 @@
 const fs = require("fs");
-const puppeteer = require('puppeteer-core');
+const puppeteer = require("puppeteer-core");
 
 let convertToPDf = async (reportUrl) => {
-  const browser = await puppeteer.connect({ browserWSEndpoint: `ws://${process.env.BROWSERLESS_ENDPOINT}` });
-  const page = await browser.newPage();
-  await page.goto(reportUrl, { waitUntil: 'networkidle2' });
-  await page.emulateMediaType('screen');
-  const pdfContent = await page.pdf({
-    format: 'Letter',
-    printBackground: true
+  const browser = await puppeteer.connect({
+    browserWSEndpoint: `ws://${process.env.BROWSERLESS_ENDPOINT}`,
   });
-
+  const page = await browser.newPage();
+  await page.goto(reportUrl, { waitUntil: "networkidle2" });
+  await page.emulateMediaType("screen");
+  const pdfContent = await page.pdf({
+    format: "Letter",
+    printBackground: true,
+  });
 
   // res.on(, function(chunk) {
   //   data.push(chunk);
@@ -27,12 +28,12 @@ let convertToPDf = async (reportUrl) => {
 
 module.exports = {
   PdfReportUrl: async function (req, res) {
-    //GRR how do we deal with this api configuration. 
+    //GRR how do we deal with this api configuration.
     //I'm not pleased with the idea of putting it in the .env.
     //this should not have to be configured it should be determined
-    const uuid = req.params.subscriptionUUID
-    const type = req.params.type
-    const cycle = req.params.cycle
+    const uuid = req.params.subscriptionUUID;
+    const type = req.params.type;
+    const cycle = req.params.cycle;
 
     const reportUrl = `http://${process.env.WEB_ENDPOINT}/reports/${type}/${uuid}/${cycle}`;
 
@@ -40,5 +41,5 @@ module.exports = {
     //res.contentType("application/pdf");
     res.setHeader("Content-Type", "application/pdf");
     res.send(pdfContent);
-  }
-}
+  },
+};
